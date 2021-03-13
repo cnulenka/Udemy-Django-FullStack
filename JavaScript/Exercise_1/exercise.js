888fba163d251fb76c177ce085096bd03cae98f2
cnulenka <sinulenka001@gmail.com>
2021-03-13 15:00:42 +0530

    javascript exersice 1

---
+++ JavaScript/Exercise_1/exercise.js
@@ -0,0 +1,5 @@
+alert("Welcome to pounds to kilograms converter!")
+var weightPound = prompt("Enter weight in pounds(lbs)")
+var weightKilogram = weightPound*0.454
+alert("Weight: "+ weightKilogram +"Kgs")
+console.log("Conversion Completed")
